056cee640490fa8d55086090afd5437668d671ad
hknunes <hknunes@gmail.com>
2023-06-01 15:28:26 +0100

    Add UI Layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,23 +1,66 @@
 import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import { Tab } from '@headlessui/react'
 import Head from 'next/head'
+import Link from 'next/link'
 
-const inter = Inter({ subsets: ['latin'] })
+const tabs = [
+  {
+    key: 'all',
+    display: 'All'
+  },
+  {
+    key: 'digital',
+    display: 'Digital'
+  },
+  {
+    key: 'Analogic',
+    display: 'Analogic'
+  }
+]
 
 export default function Home() {
   return (
-    <div>
-        <Head>
+    <div className="flex flex-col h-full bg-[url('/photography-bg.jpg')] bg-top bg-cover">
+      <Head>
         <title>Home</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className=''>
-        <p>Placholder for main</p>
+      <header className='flex justify-between items-center h-[90px] px-6'>
+        <div>Photography Portfolio</div>
+        <Link href="#" className='rounded-3xl bg-slate-200 text-stone-600 px-3 py-2 hover:bg-opacity-90'>
+          {/* <button className='rounded-3xl bg-slate-200 text-stone-600 px-3 py-2 hover:bg-opacity-90'> */}
+            Get in touch
+          {/* </button> */}
+        </Link>
+      </header>
+
+      <main className='grow'>
+        <div className='flex flex-col items-center h-full'>
+          <Tab.Group>
+            <Tab.List className='flex items-center gap-12 '>
+
+              {tabs.map(tab => (
+                <Tab key={tab.key} className='p-2'>
+                  {({selected}) => (
+                    <span className={selected ? 'text-white':'text-stone-600'}>
+                      {tab.display}
+                    </span>
+                  )}
+                </Tab>
+              ))}
+            </Tab.List>
+            <Tab.Panels className='h-full bg-stone-900 bg-opacity-80 max-w-[900px] w-full p-2 sm:p-4 my-6'>
+              <Tab.Panel className=''>All Content</Tab.Panel>
+              <Tab.Panel>Digital Content</Tab.Panel>
+              <Tab.Panel>Analogic Content</Tab.Panel>
+            </Tab.Panels>
+          </Tab.Group>
+        </div>
       </main>
 
-      <footer>
-        <p>Placholder for footer</p>
+      <footer className='h-[60px] flex justify-center items-center'>
+        <p>Photography Portfolio</p>
       </footer>
     </div>
   )
