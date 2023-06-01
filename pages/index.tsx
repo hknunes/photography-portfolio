f800595e30dd2d9390eb70c97053b6c3772fa6f7
hknunes <hknunes@gmail.com>
2023-06-01 16:28:22 +0100

    Add Masonry

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,8 @@ import Image from 'next/image'
 import { Tab } from '@headlessui/react'
 import Head from 'next/head'
 import Link from 'next/link'
+import Masonry from 'react-masonry-css'
+import classNames from 'classnames'
 
 const tabs = [
   {
@@ -20,38 +22,48 @@ const tabs = [
 
 export default function Home() {
   return (
-    <div className="flex flex-col h-full bg-[url('/photography-bg.jpg')] bg-top bg-cover">
+    <div className="h-full bg-[url('/photography-bg.jpg')] bg-top bg-cover overflow-auto">
       <Head>
         <title>Home</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <header className='flex justify-between items-center h-[90px] px-6'>
+      <header className='fixed top-0 w-full z-10 flex justify-between items-center h-[90px] px-6'>
         <div>Photography Portfolio</div>
-        <Link href="#" className='rounded-3xl bg-slate-200 text-stone-600 px-3 py-2 hover:bg-opacity-90'>
-          {/* <button className='rounded-3xl bg-slate-200 text-stone-600 px-3 py-2 hover:bg-opacity-90'> */}
-            Get in touch
-          {/* </button> */}
+        <Link href="#" className='hidden rounded-3xl bg-slate-200 text-stone-600 px-3 py-2 hover:bg-opacity-90'>
+          Get in touch
         </Link>
       </header>
 
-      <main className='grow'>
+      <main className='pt-[110px]'>
         <div className='flex flex-col items-center h-full'>
           <Tab.Group>
-            <Tab.List className='flex items-center gap-12 '>
+            <Tab.List className='flex items-center gap-12'>
 
               {tabs.map(tab => (
                 <Tab key={tab.key} className='p-2'>
                   {({selected}) => (
-                    <span className={selected ? 'text-white':'text-stone-600'}>
+                    <span className={classNames('uppercase', selected ? 'text-white':'text-stone-600')}>
                       {tab.display}
                     </span>
                   )}
                 </Tab>
               ))}
             </Tab.List>
-            <Tab.Panels className='h-full bg-stone-900 bg-opacity-80 max-w-[900px] w-full p-2 sm:p-4 my-6'>
-              <Tab.Panel className=''>All Content</Tab.Panel>
+            <Tab.Panels className='h-full max-w-[900px] w-full p-2 sm:p-4 my-6'>
+              <Tab.Panel>
+                <Masonry  
+                  breakpointCols={2} 
+                  className="flex gap-4" 
+                  columnClassName=""
+                >
+                  <img src='/ocean-1.jpeg' alt='ocean-1' className='my-4'/>
+                  <img src='/ocean-2.jpeg' alt='ocean-2' className='my-4'/>
+                  <img src='/ocean-3.jpeg' alt='ocean-3' className='my-4'/>
+                  <img src='/ocean-4.jpeg' alt='ocean-4' className='my-4'/>
+                  <img src='/ocean-5.jpeg' alt='ocean-5' className='my-4'/>
+                </Masonry>
+              </Tab.Panel>
               <Tab.Panel>Digital Content</Tab.Panel>
               <Tab.Panel>Analogic Content</Tab.Panel>
             </Tab.Panels>
@@ -59,7 +71,7 @@ export default function Home() {
         </div>
       </main>
 
-      <footer className='h-[60px] flex justify-center items-center'>
+      <footer className='h-[90px] flex justify-center items-center uppercase text-lg font-medium'>
         <p>Photography Portfolio</p>
       </footer>
     </div>
